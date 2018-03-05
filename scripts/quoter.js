6ebba232842f350f8776eb742b2f7d1c38fb29d9
unknown <swartalow@gmail.com>
2018-03-05 13:56:20 -0500

    tweet out added

--- scripts/quoter.js
+++ scripts/quoter.js
@@ -14,19 +14,27 @@ function starter(){
         e : "bolek"
 }
 document.getElementById("btn-qt").onclick = displayQuote;
-
+document.getElementById("btn-tw").onclick = tweetQuote;
+//getRandomInt returns random number from 0 to max;
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+//getRandom returns random quote from quotes set;
 function getRandom(quotes){
     var keyArr = Object.keys(quotes);
     var keyArrLen = keyArr.length;
     return quotes[keyArr[getRandomInt(keyArrLen)]];
 }
 var quote_index = getRandom(quotes);
+//displayQuote changes quote displayed;
 function displayQuote() {
     document.getElementById("qtholder").innerHTML = quote_index;
+}
+
+function tweetQuote(){
+    var twtLink = 'http://twitter.com/home?status=' +encodeURIComponent(quote_index);
+ window.open(twtLink,'_blank');
 }
 }
 ready(starter);
