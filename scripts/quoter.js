69f49bbcdf9fda4ff8370ea0771ac968b9c3e798
unknown <swartalow@gmail.com>
2018-03-05 12:27:58 -0500

    quotes get at random

--- scripts/quoter.js
+++ scripts/quoter.js
@@ -1,11 +1,21 @@
 var quotes = {
     a : "abcd",
     b : "bcda",
-    c : "huiec"
+    c : "huiec",
+    d : "stolec",
+    e : "bolek"
 }
 
 document.getElementById("btn-qt").onclick = displayQuote;
+function getRandom(quotes){
+    var keyArr = Object.keys(quotes);
+    var keyArrLen = keyArr.length;
 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    return quotes[keyArr[getRandomInt(keyArrLen)]];
+}
 function displayQuote() {
-    document.getElementById("qtholder").innerHTML = quotes["a"];
+    document.getElementById("qtholder").innerHTML = getRandom(quotes);
 }
