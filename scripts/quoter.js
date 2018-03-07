d8c1402e63c06a6fadf12ce3b85a000227992d97
unknown <swartalow@gmail.com>
2018-03-07 16:30:06 -0500

    quotes get from third-party

--- scripts/quoter.js
+++ scripts/quoter.js
@@ -5,39 +5,37 @@ function ready(fn) {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
-
-    var quotes = {
-        a : "abcd",
-        b : "bcda",
-        c : "huiec",
-        d : "stolec",
-        e : "bolek"
-}
-function starter(){
-document.getElementById("btn-qt").onclick = displayQuote;
-document.getElementById("btn-tw").onclick = tweetQuote;
-}
-//getRandomInt returns random number from 0 to max;
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
 }
 //getRandom returns random quote from quotes set;
-function getRandomQuotes(quotes){
-    var keyArr = Object.keys(quotes);
-    var keyArrLen = keyArr.length;
-    var quoteIndex = keyArr[getRandomInt(keyArrLen)]
-    return quotes[quoteIndex];
+function getRandomQuotes(){
+    var currentQuote = "", currentAuthor = ""
+    var request = new XMLHttpRequest();
+    request.open('GET', "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1", true);
+    request.setRequestHeader("X-Mashape-Key", "A6EFpJ2oCQmshDaNl1pgKgMEfT9pp1DzSYwjsnrign0RheBPlH")
+    request.onload = function() {
+        if(request.status >= 200 && request.status < 400){
+            var data = JSON.parse(this.response);
+            (function displayQuote() {
+                document.getElementById("qtholder").innerHTML = data["quote"];
+                document.getElementById("author").innerHTML = data["author"];
+            })();
+        }else {
+      console.log("Error!")
+        }
+      };
+    request.send();
 }
 //displayQuote changes quote displayed;
-function displayQuote() {
-    document.getElementById("qtholder").innerHTML = getRandomQuotes(quotes);
-}
+
 //tweetQuote tweets quote
 function tweetQuote(){
     var twtext = document.getElementById("qtholder").innerHTML;
     var twtLink = 'http://twitter.com/home?status=' +(twtext);
-    console.log(twtLink);
- window.open(twtLink,'_blank');
+    window.open(twtLink,'_blank');
+}
+function starter(){
+document.getElementById("btn-qt").onclick = getRandomQuotes;
+document.getElementById("btn-tw").onclick = tweetQuote;
 }
 
 ready(starter);
